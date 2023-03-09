753de7cefe9b93e286bfa24a796be88e5a4f004f
christian.sonic <yoh.chrisitan@gmail.com>
2023-03-08 22:15:18 -0400

    Initial Commit

---
+++ RideOrDies/RideOrDiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideOrDiesApp.swift
+//  RideOrDies
+//
+//  Created by Sonic on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RideOrDiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
